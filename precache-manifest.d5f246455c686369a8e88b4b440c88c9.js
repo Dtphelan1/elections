10f050cad318120b8a2be020992e1c8e5fe27a97
Dylan Phelan <dtphelan1@gmail.com>
2020-08-03 23:54:05 -0400

    Updates

---
+++ precache-manifest.d5f246455c686369a8e88b4b440c88c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab89cfbcebc35bbd6d018d576aeeeb4",
+    "url": "/electionsandtournaments/index.html"
+  },
+  {
+    "revision": "fba9e147c3105545e775",
+    "url": "/electionsandtournaments/static/css/main.628fa948.chunk.css"
+  },
+  {
+    "revision": "7b141a6ffbcea266bcfd",
+    "url": "/electionsandtournaments/static/js/2.3ab96c13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/electionsandtournaments/static/js/2.3ab96c13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba9e147c3105545e775",
+    "url": "/electionsandtournaments/static/js/main.b1568d01.chunk.js"
+  },
+  {
+    "revision": "f5fdddf1d6b72ad884ea",
+    "url": "/electionsandtournaments/static/js/runtime-main.537a2279.js"
+  },
+  {
+    "revision": "69f1766d05d56f6f4efa6d319664b794",
+    "url": "/electionsandtournaments/static/media/incomplete-tournament.69f1766d.svg"
+  }
+]);
