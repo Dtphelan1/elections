0e40194c12b5219e2a79ed7bd8aae8dd2992d466
Dylan Phelan <dtphelan1@gmail.com>
2020-08-07 20:44:30 -0400

    Obvious improvement

--- src/Footer.js
+++ src/Footer.js
@@ -11,7 +11,7 @@ function Footer() {
         <footer id="footer">
             <div className="container d-flex justify-content-center">
                 <span>
-                    Created by Dylan Phelan
+                    <a href='https://blacklivesmatters.carrd.co/'>Black Lives Matter </a>
                     | <a href='https://github.com/Dtphelan1?tab=repositories'><FontAwesomeIcon icon={faGithub} /> @dtphelan1 </a>
                     | <a href='https://twitter.com/Dtphelan1'><FontAwesomeIcon icon={faTwitter} /> @dtphelan1 </a>
                 </span>
